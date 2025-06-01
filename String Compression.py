a8c9778cf046ab1a0ab434c69fcc858d863eda6e
Abhishek Narwar <142799395+abhisheknarwar0042@users.noreply.github.com>
2025-06-01 10:30:38 +0530

    Update String Compression.py

--- String Compression.py
+++ String Compression.py
@@ -1,40 +1,31 @@
-def compress(s):
-    """
-    This solution compresses without checking. Known as the RunLength Compression algorithm.
-    """
-    
-    # Begin Run as empty string
-    r = ""
-    l = len(s)
-    
-    # Check for length 0
-    if l == 0:
-        return ""
-    
-    # Check for length 1
-    if l == 1:
-        return s + "1"
-    
-    #Intialize Values
-    last = s[0]
-    cnt = 1
-    i = 1
-    
-    while i < l:
+class Solution:
+    def compress(self, chars):
+        write = 0  # Write index
+        read = 0   # Read index
         
-        # Check to see if it is the same letter
-        if s[i] == s[i - 1]: 
-            # Add a count if same as previous
-            cnt += 1
-        else:
-            # Otherwise store the previous data
-            r = r + s[i - 1] + str(cnt)
-            cnt = 1
+        while read < len(chars):
+            char = chars[read]
+            count = 0
             
-        # Add to index count to terminate while loop
-        i += 1
-    
-    # Put everything back into run
-   r = r + s[i - 1] + str(cnt)
-    
-  return r
+            # Count occurrences of the current character
+            while read < len(chars) and chars[read] == char:
+                read += 1
+                count += 1
+            
+            # Write the character
+            chars[write] = char
+            write += 1
+            
+            # Write the count if more than 1
+            if count > 1:
+                for digit in str(count):
+                    chars[write] = digit
+                    write += 1
+        
+        return write
+chars = ["a","a","b","b","c","c","c"]
+sol = Solution()
+length = sol.compress(chars)
+
+print(length)          # Output: 6
+print(chars[:length])  # Output: ['a', '2', 'b', '2', 'c', '3']
